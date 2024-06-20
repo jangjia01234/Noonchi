b63c6b226f768b1eac3f91a690556f4bb22625cf
Jia <71865277+jangjia01234@users.noreply.github.com>
2024-06-21 07:47:39 +0900

    :sparkles: 1차 업로드

--- Noonchi/ViewController.swift
+++ Noonchi/ViewController.swift
@@ -11,7 +11,7 @@ import EyeTracking
 import AVKit
 
 class ViewController: UIViewController, ARSessionDelegate {
-    //    var eyeTracking: EyeTracking?
+    var eyeTracking: EyeTracking?
     var session: ARSession!
     var isBlinkDetected: Bool = false
     var leftEyeValue: Double = 0
@@ -29,49 +29,52 @@ class ViewController: UIViewController, ARSessionDelegate {
     var blinkDuration: TimeInterval = 0.0
     let blinkThreshold: TimeInterval = 0.2
     
-    let imageView: UIImageView = {
-        let aImageView = UIImageView()
-        aImageView.backgroundColor = .red
-        aImageView.image = UIImage(named: "mainImage")
-        aImageView.translatesAutoresizingMaskIntoConstraints = false
-        return aImageView
+    lazy var label: UILabel = {
+        let label: UILabel = UILabel()
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 28, weight: .bold)
+        label.text = "오른쪽으로\n곁눈질 👀 해서\n영상을 넘겨보세요!"
+        label.numberOfLines = 3
+        label.layer.masksToBounds = true
+        label.textAlignment = .left
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        let myImage: UIImage = UIImage(named: "mainImage.png")!
+        imageView.image = myImage
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    lazy var videoButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("시작하기", for: .normal)
+        button.backgroundColor = UIColor(red: 0/255, green: 114/255, blue: 190/255, alpha: 1.0)
+        button.setTitleColor(.white, for: .normal)
+        button.addTarget(self, action: #selector(playVideo), for: .touchUpInside)
+        button.layer.cornerRadius = 10
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupARSession()
+        setupConstraints()
         
-        view.addSubview(imageView)
+        let configuration = Configuration(appID: "ios-eye-tracking-video")
+        eyeTracking = EyeTracking(configuration: configuration)
         
-        NSLayoutConstraint.activate([
-            imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            imageView.widthAnchor.constraint(equalToConstant: 200),
-            imageView.heightAnchor.constraint(equalToConstant: 230),
-        ])
+        eyeTracking?.pointer.backgroundColor = .blue
         
-        //        let configuration = Configuration(appID: "ios-eye-tracking-video", blendShapes: [.eyeBlinkLeft, .eyeBlinkRight])
-        //        eyeTracking = EyeTracking(configuration: configuration)
-        
-        let endButton = UIButton(type: .system)
-        endButton.setTitle("End Session", for: .normal)
-        endButton.addTarget(self, action: #selector(endSession), for: .touchUpInside)
-        endButton.frame = CGRect(x: 20, y: 100, width: 200, height: 50)
-        self.view.addSubview(endButton)
-        
-        //        eyeTracking?.pointer.backgroundColor = .blue
-        //        eyeTracking?.displayScanpath(for: "8136AD7E-7262-4F07-A554-2605506B985D", animated: true)
-        
-        let videoButton = UIButton(type: .system)
-        videoButton.setTitle("Watch Video", for: .normal)
-        videoButton.addTarget(self, action: #selector(playVideo), for: .touchUpInside)
-        videoButton.frame = CGRect(x: 20, y: 150, width: 200, height: 50)
-        self.view.addSubview(videoButton)
-        
-        // displayLink = CADisplayLink(target: self, selector: #selector(checkGazePosition))
+        // MARK: - 타겟 설정
+        displayLink = CADisplayLink(target: self, selector: #selector(checkGazePosition))
         // displayLink = CADisplayLink(target: self, selector: #selector(checkBlink))
-        displayLink = CADisplayLink(target: self, selector: #selector(checkEyeMotion))
+        //        displayLink = CADisplayLink(target: self, selector: #selector(checkEyeMotion))
         displayLink?.add(to: .current, forMode: .default)
     }
     
@@ -84,9 +87,40 @@ class ViewController: UIViewController, ARSessionDelegate {
         session.run(configuration, options: [])
     }
     
+    func setupConstraints() {
+        self.view.backgroundColor = UIColor.white
+        
+        self.view.addSubview(label)
+        self.view.addSubview(imageView)
+        self.view.addSubview(videoButton)
+        
+        self.view.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            // MARK: - 상단 텍스트
+            label.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 0),
+            label.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: -150),
+            label.bottomAnchor.constraint(equalTo: imageView.topAnchor),
+            label.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 40),
+            
+            // MARK: - 중앙 이미지
+            imageView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 0),
+            imageView.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 0),
+            imageView.bottomAnchor.constraint(equalTo: videoButton.topAnchor, constant: -200),
+            
+            // MARK: - 하단 버튼
+            videoButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 0),
+            videoButton.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 0),
+            videoButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
+            videoButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20),
+            videoButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20),
+            videoButton.heightAnchor.constraint(equalToConstant: 60)
+        ])
+    }
+    
     @objc func startSession() {
-        //        eyeTracking?.startSession()
-        //        eyeTracking?.showPointer()
+        eyeTracking?.startSession()
+        eyeTracking?.showPointer()
         
         let configuration = ARFaceTrackingConfiguration()
         configuration.isLightEstimationEnabled = true
@@ -94,8 +128,8 @@ class ViewController: UIViewController, ARSessionDelegate {
     }
     
     @objc func endSession() {
-        //        eyeTracking?.hidePointer()
-        //        eyeTracking?.endSession()
+        eyeTracking?.hidePointer()
+        eyeTracking?.endSession()
         
         session.pause()
     }
@@ -155,9 +189,9 @@ class ViewController: UIViewController, ARSessionDelegate {
         player?.replaceCurrentItem(with: playerItem)
         player?.play()
         
-        //        if (eyeTracking?.currentSession) != nil {
-        //            endSession()
-        //        }
+        if (eyeTracking?.currentSession) != nil {
+            endSession()
+        }
         
         startSession()
     }
@@ -167,37 +201,37 @@ class ViewController: UIViewController, ARSessionDelegate {
             return
         }
         
-        leftEyeValue = faceAnchor.blendShapes[.eyeBlinkLeft]?.doubleValue ?? 0.0
-        rightEyeValue = faceAnchor.blendShapes[.eyeBlinkRight]?.doubleValue ?? 0.0
+        //        leftEyeValue = faceAnchor.blendShapes[.eyeBlinkLeft]?.doubleValue ?? 0.0
+        //        rightEyeValue = faceAnchor.blendShapes[.eyeBlinkRight]?.doubleValue ?? 0.0
         
         //        isBlinkDetected = (leftEyeValue > 0.3 && rightEyeValue > 0.3)
-        let currentBlinkDetected = (leftEyeValue > 0.3 && rightEyeValue > 0.3)
-        
-        if currentBlinkDetected && !isEyesClosed {
-            isEyesClosed = true
-            blinkTimer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { timer in
-                self.blinkDuration += 0.1
-            }
-        } else if !currentBlinkDetected && isEyesClosed {
-            isEyesClosed = false
-            blinkTimer?.invalidate()
-            blinkTimer = nil
-            blinkDuration = 0.0
-        }
+        //        let currentBlinkDetected = (leftEyeValue > 0.3 && rightEyeValue > 0.3)
+        //
+        //        if currentBlinkDetected && !isEyesClosed {
+        //            isEyesClosed = true
+        //            blinkTimer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { timer in
+        //                self.blinkDuration += 0.1
+        //            }
+        //        } else if !currentBlinkDetected && isEyesClosed {
+        //            isEyesClosed = false
+        //            blinkTimer?.invalidate()
+        //            blinkTimer = nil
+        //            blinkDuration = 0.0
+        //        }
     }
     
     // MARK: - 오른쪽으로 곁눈질할 경우 다음 영상 재생
-    //    @objc func checkGazePosition() {
-    //        guard let currentSession = eyeTracking?.currentSession, let lastGaze = currentSession.scanPath.last else { return }
-    
-    //        // MARK: - 화면 오른쪽 밖으로 나갔는지 확인
-    //        // 다른 방향으로 설정하고 싶으면 UIScreen.main.bounds.width 활용!
-    //        if lastGaze.x < 0 {
-    //            print("Gaze exited to the right")
-    //            endSession()
-    //            playNextVideo()
-    //        }
-    //    }
+    @objc func checkGazePosition() {
+        guard let currentSession = eyeTracking?.currentSession, let lastGaze = currentSession.scanPath.last else { return }
+        
+        // MARK: - 화면 오른쪽 밖으로 나갔는지 확인
+        // 다른 방향으로 설정하고 싶으면 UIScreen.main.bounds.width 활용!
+        if lastGaze.x < 0 {
+            print("Gaze exited to the right")
+            endSession()
+            playNextVideo()
+        }
+    }
     
     //    @objc func checkBlink() {
     //        if isBlinkDetected {
@@ -208,31 +242,31 @@ class ViewController: UIViewController, ARSessionDelegate {
     //        }
     //    }
     
-    @objc func checkEyeMotion() {
-        //        guard let currentSession = eyeTracking?.currentSession, let lastGaze = currentSession.scanPath.last else { return }
-        
-        //        DispatchQueue.main.async {
-        // Check gaze position
-        //            if lastGaze.x < 0 {
-        //                print("Gaze exited to the right")
-        //                self.endSession()
-        //                self.playNextVideo()
-        //            }
-        
-        // Check blink
-        //            if self.isBlinkDetected {
-        //                self.player?.pause()
-        //                print("Video paused due to blink detection")
-        //            } else {
-        //                self.player?.play()
-        //            }
-        //        }
-        
-        if isBlinkDetected {
-            player?.pause()
-            print("Video paused due to blink detection")
-        } else {
-            player?.play()
-        }
-    }
+    //    @objc func checkEyeMotion() {
+    //        guard let currentSession = eyeTracking?.currentSession, let lastGaze = currentSession.scanPath.last else { return }
+    
+    //        DispatchQueue.main.async {
+    // Check gaze position
+    //            if lastGaze.x < 0 {
+    //                print("Gaze exited to the right")
+    //                self.endSession()
+    //                self.playNextVideo()
+    //            }
+    
+    // Check blink
+    //            if self.isBlinkDetected {
+    //                self.player?.pause()
+    //                print("Video paused due to blink detection")
+    //            } else {
+    //                self.player?.play()
+    //            }
+    //        }
+    
+    //        if isBlinkDetected {
+    //            player?.pause()
+    //            print("Video paused due to blink detection")
+    //        } else {
+    //            player?.play()
+    //        }
+    //    }
 }
